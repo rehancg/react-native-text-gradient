e4e3eabb4518ca2a797d44acc4bff76ef61fd27a
Rehan Chanaka <rehang@99x.lk>
2020-02-16 10:16:14 +0530

    updated patch

--- patch-rn.js
+++ patch-rn.js
@@ -4,19 +4,21 @@ const { writeFile, readFile, readdir } = require('fs');
 const { promisify } = require('util');
 const path = require('path');
 
-const folder = 'node_modules/react-native/Libraries/Renderer/oss/';
+const folder = 'node_modules/react-native/Libraries/Renderer/implementations/';
 
 const pattern = new RegExp(
-  'invariant\\([\\s\\S]{0,20}' +
-  '(hostContext|type)\\.isInAParentText,[\\s\\S]{0,20}' +
-  '"Text strings must be rendered within a <Text> component\\."[\\s\\S]{0,20}' +
-  '\\)[;,]'
-);
+  'throw ReactError[\\s\\S]{0,80}Text strings must be rendered within a <Text> component[\\s\\S]{0,80}[^()]\\)[;,]'
+  , 'gm');
 
 const patchFile = async (file) => {
   const content = (await promisify(readFile)(file)).toString();
-  const patched = content.replace(pattern, '');
-
+  const patched = content.replace(pattern, () => {
+    const fileRegEx = new RegExp('dev');
+    if (fileRegEx.test(file))
+      return '';
+    else
+      return '{}';
+  });
   await promisify(writeFile)(file, patched);
 };
 
